53267d7b85713a70520773f30a59856092abcac9
yangchen <yangchen625@163.com>
2019-06-08 00:30:04 +0800

    路由，终于搞清楚了，狗日的

---
+++ src/Goodlists/cart.vue
@@ -0,0 +1,34 @@
+/** create by yangchen  2019/6/7 23:37
+* @description：
+*/
+<template>
+    <div> cart</div>
+</template>
+
+<script>
+    export default {
+        name: "cart",
+        data() {
+            return {msg: ''}
+        },
+        watch: {
+            'msg': function (val, oldVal) {
+                if (val !== oldVal) {
+                    console.log(val)
+                }
+            }
+        },
+        methods: {
+            componentStart() {
+                console.log(" cart mounted ")
+            }
+        },
+        mounted() {
+            this.componentStart()
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,17 @@
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
 const routers = [
-    {
-        path: '/',
-        meta: {
-            title: ''
-        },
-        component: (resolve) => require(['./views/index.vue'], resolve)
-    }
+    // {
+    //     path: '/',
+    //     meta: {
+    //         title: ''
+    //     },
+    //     component: (resolve) => require(['./views/index.vue'], resolve)
+    // },
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar }
+
+
 ];
 export default routers;
